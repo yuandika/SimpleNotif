c08b89a6db1a31ac84e139c3e0e5e297626b14ab
Yuandika Alfahreiza <yuandika11@gmail.com>
2021-12-12 14:18:18 +0700

    Notification with NotificationBuilder and NotificationManager

--- app/src/main/java/com/dicoding/simplenotif/MainActivity.kt
+++ app/src/main/java/com/dicoding/simplenotif/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.dicoding.simplenotif
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //aksi untuk onClick pada button
+    fun sendNotification(view: View) {
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notifications_white_48px)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications_white_48px))
+            .setContentTitle(resources.getString(R.string.content_title))
+            .setContentText(resources.getString(R.string.content_text))
+            .setSubText(resources.getString(R.string.subtext))
+            .setAutoCancel(true)
+
+        /*
+        Untuk android Oreo ke atas perlu menambahkan notification channel
+        */
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            /* Create or update. */
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = CHANNEL_NAME
+
+            mBuilder.setChannelId(CHANNEL_ID)
+            mNotificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = mBuilder.build()
+
+        mNotificationManager.notify(NOTIFICATION_ID, notification)
+    }
+    companion object {
+        private const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "channel_01"
+        private const val CHANNEL_NAME = "dicoding channel"
     }
 }
